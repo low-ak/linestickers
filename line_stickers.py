b5c98b7630624a1334f1fa1a7d1e354a59359c1a
Lowak <71454472+low-ak@users.noreply.github.com>
2020-09-20 19:35:15 +0200

    Create line_stickers.py

---
+++ line_stickers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import requests
+import re
+import bs4
+
+if(len(sys.argv) < 2):
+    print ('No argument, usage "./line_stickers.py [url]"')
+    exit(-1)
+
+url = sys.argv[1:2][0]
+
+main = bs4.BeautifulSoup(requests.get(url).content, 'html.parser')
+
+title = main.find('p', {'class': 'mdCMN38Item01Ttl'}).text
+tar_dir = 'stickers_' + '_'.join(title.split())
+
+print('Downloading "'+title+'" to', tar_dir)
+
+if not os.path.exists(tar_dir):
+    os.makedirs(tar_dir)
+
+stickers = main.findAll('span', {'class', 'mdCMN09Image'})
+
+urlm = re.compile(r'background-image:url\((.*);compress=true\)')
+namem = re.compile(r'.*sticker/([0-9]*).*')
+for sticker in stickers:
+    img_url = urlm.search(sticker['style']).group(1)
+    name = namem.search(img_url).group(1)
+    os.system('curl -o '+tar_dir+'/'+name+'.png '+img_url)
+    
